cf842013741270319e1d9024c00bcb468a595fac
BloxSwaGaming <75168534+BloxSwaGaming@users.noreply.github.com>
2022-05-20 15:17:22 +0530

    Add files via upload

---
+++ Snake/Scripts/game.js
@@ -0,0 +1,52 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection, snakeBody } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
+
+let lastRenderTime = 0
+const gameBoard = document.getElementById('game-board')
+let gameOver = false
+
+function main(currentTime) {
+    if (gameOver) {
+        if (confirm('You died!')) {
+            window.location = '/'
+        }
+        return
+    }
+
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+    lastRenderTime = currentTime
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function updateScore() {
+    let counter = -1
+    snakeBody.forEach(e => {
+        counter = counter + 1
+    })
+    document.getElementById('score').innerHTML = "Score: " + counter;
+}
+
+function update() {
+    updateSnake()
+    updateFood()
+    checkDeath()
+    updateScore()
+}
+
+function draw() {
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
